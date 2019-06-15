d04ab39949fd2ce909d6cd21bcc3e2fc0d251b8c
Stephen Privitera <sprivite@gmail.com>
2019-06-15 11:29:37 +0200

    map

--- index.py
+++ index.py
@@ -1,7 +1,14 @@
 from flask import Flask, Response
+import folium
+
 app = Flask(__name__)
+
+
+m = folium.Map(location=[48.7, 13.8], zoom_start=5)
+m.save('test.html')
+t = open('test.html').read()
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    return t
