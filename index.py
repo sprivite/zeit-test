d9910acb10dfed25fcfa99ce7e2b39c1c6d2537d
Stephen Privitera <sprivite@gmail.com>
2019-06-22 20:19:10 +0200

    prettify

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import pandas as pd
 app = Flask(__name__)
 
 global_counter = 0
-chosen_countries = []
+chosen_countries = set([])
 country_centroids = pd.read_csv('country_centroids.csv.gz', index_col='name')
 countries = list(country_centroids.index)
 map_template = Template(open('country.html').read())
@@ -15,7 +15,7 @@ map_template = Template(open('country.html').read())
 @app.route('/', methods=['GET'])
 def viewer():
 
-    global global_counter
+    global global_counter, chosen_countries
     global_counter += 1
 
     country = request.args.get('country')
@@ -23,6 +23,8 @@ def viewer():
         response = map_template.render(
             country='Enter a country!',
             zoom=3,
+            n_found=len(chosen_countries),
+            n_countries=len(countries),
             latitude=52,
             longitude=13,
             countries='<br>'.join(sorted(chosen_countries)))
@@ -32,13 +34,15 @@ def viewer():
     if country not in countries:
         return Response('Invalid country: {}!'.format(country), status=400)
         
-    chosen_countries.append(country)
+    chosen_countries |= set([country])
 
     center = country_centroids.loc[country]
     lat, lon = center.latitude, center.longitude
     response = map_template.render(
         country=country,
         zoom=5,
+        n_found=len(chosen_countries),
+        n_countries=len(countries),
         longitude=lon,
         latitude=lat,
         countries='<br>'.join(sorted(chosen_countries)))
